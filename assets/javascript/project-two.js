aa172d24ec4478f6f4d00aabb28c80a80c17529d
Gabi <gabi.cassarino@gmail.com>
2018-04-07 13:16:53 -0400

    updated project

--- assets/javascript/project-two.js
+++ assets/javascript/project-two.js
@@ -1,15 +1,16 @@
 var recommendationCounter = 0;
 var numRecommendations;
 var queryUrl;
+var categoryCounter = 0;
 
-function runThis(numRecommendations) {}
+function runThis(numRecommendations) { }
 
 // var url = "https://api.foursquare.com/v2/search/recommendations?near=" + locationChosen + "&v=20160607&client_id=" + clientID + "&client_secret=" + clientSecret + "";
 
 var locationChosen = "New York, NY";
-var clientID =  "Y0THHOXVAO3E2J32CKEOXSGYSZ2ACS4NZU5J4ABSYVOS5I50"
-var clientSecret = "JZGTVKXP4H3E4IROULCOUJR4KBOXRY24TCZ4QS0DILORD3RI"
-
+var clientID = "Y0THHOXVAO3E2J32CKEOXSGYSZ2ACS4NZU5J4ABSYVOS5I50";
+var clientSecret = "JZGTVKXP4H3E4IROULCOUJR4KBOXRY24TCZ4QS0DILORD3RI";
+var categoryNum = "4d4b7104d754a06370d81259,4d4b7105d754a06372d81259,4d4b7105d754a06373d81259,4d4b7105d754a06374d81259,4d4b7105d754a06376d81259,4d4b7105d754a06377d81259,4d4b7105d754a06375d81259,4e67e38e036454776db1fb3a,4d4b7105d754a06378d81259,4d4b7105d754a06379d81259";
 // queryUrl += '?' + $.param({
 //     'clientID': "Y0THHOXVAO3E2J32CKEOXSGYSZ2ACS4NZU5J4ABSYVOS5I50",
 //     'clientSecret': "JZGTVKXP4H3E4IROULCOUJR4KBOXRY24TCZ4QS0DILORD3RI",
@@ -17,30 +18,55 @@ var clientSecret = "JZGTVKXP4H3E4IROULCOUJR4KBOXRY24TCZ4QS0DILORD3RI"
 
 // });
 
-queryUrl = "https://api.foursquare.com/v2/search/recommendations?near=" + locationChosen + "&v=20160607&client_id=" + clientID + "&client_secret=" + clientSecret + "";
+queryUrl = "https://api.foursquare.com/v2/search/recommendations?near=" + locationChosen + "&v=20160607&sortByDistance=1&venuePhotos=1&categoryId=" + categoryNum + "&client_id=" + clientID + "&client_secret=" + clientSecret + "";
+
+
+// //$("#location").on("click", function () {
+//     locationChosen
+//         }
 
 $.ajax({
     url: queryUrl,
     method: 'GET',
 }).done(function (result) {
     console.log(result);
+    console.log(result.response.group.results[0].venue.name);
+    console.log(result.response.group.results[0].photo);
+    console.log(result.response.group.results[0].venue.categories[0].name);
 
-    for (var i = 0; i < numRecommendations; i++) {
+    $("#recSearch").on("click", function () {
+        var userSelection = $("input[type='checkbox']").val();
+        //if ($(this).is(':checked')) {
+        //userSelection = $("<p>").text(result.response.group.results[j].venue.categories[0].name);
+        //userSelection.addClass("selection");
+        //userSelection.attr("id" + categoryCounter);
+        if (userSelection === "4d4b7104d754a06370d81259") {
+            for (var j = 0; j <= 9; j++) {
+                categoryCounter++;
 
-        recommendationCounter++;
-
-        var recSection = $("<div>");
-        recSection.addClass("rec");
-        recSection.attr("id", "article-rec-" + recommendationCounter);
-        $("#rec-section").append(recSection);
-    }
-
-
+                //var userSelection = $("<p>").text(result.response.group.results[j].venue.categories[0].name);
+                //userSelection.addClass("selection");
+                //userSelection.attr("id" + categoryCounter);
+            }
+        } else {
+            console.log(result);
+        }
 
 
+        for (var i = 0; i <= 9; i++) {
 
+            recommendationCounter++;
+
+            var recommendationSection = $("<p>").text(result.response.group.results[i].venue.name);
+            recommendationSection.addClass("recs");
+            recommendationSection.attr("id", "article-well-" + recommendationCounter);
+
+        }
+        $("#rec-section").append(recommendationSection);
+    });
 
 }).fail(function (err) {
     console.log(queryUrl);
     throw err;
 });
+
--- assets/javascript/project.js
+++ assets/javascript/project.js
@@ -1,42 +1,463 @@
 var map;
 var locationChosen;
 var geocoder;
+var service;
+var infowindow;
 
 
-
+//Load map when the page is fisrt loaded
 function initMap() {
-    
-    var latlng = new google.maps.LatLng(-34.397, 150.644);
+
+    var latlng = new google.maps.LatLng(40.7128, - 74.0060);
     var mapOptions = {
         zoom: 12,
-        center: latlng
+        center: latlng,
+        styles: [
+            {
+              "elementType": "geometry",
+              "stylers": [
+                {
+                  "color": "#ebe3cd"
+                },
+                {
+                  "visibility": "simplified"
+                }
+              ]
+            },
+            {
+              "elementType": "labels.text",
+              "stylers": [
+                {
+                  "visibility": "on"
+                }
+              ]
+            },
+            {
+              "elementType": "labels.text.fill",
+              "stylers": [
+                {
+                  "color": "#523735"
+                }
+              ]
+            },
+            {
+              "elementType": "labels.text.stroke",
+              "stylers": [
+                {
+                  "color": "#f5f1e6"
+                }
+              ]
+            },
+            {
+              "featureType": "administrative",
+              "elementType": "geometry.stroke",
+              "stylers": [
+                {
+                  "color": "#c9b2a6"
+                }
+              ]
+            },
+            {
+              "featureType": "administrative.land_parcel",
+              "elementType": "geometry.stroke",
+              "stylers": [
+                {
+                  "color": "#dcd2be"
+                }
+              ]
+            },
+            {
+              "featureType": "administrative.land_parcel",
+              "elementType": "labels.text.fill",
+              "stylers": [
+                {
+                  "color": "#ae9e90"
+                }
+              ]
+            },
+            {
+              "featureType": "landscape.natural",
+              "elementType": "geometry",
+              "stylers": [
+                {
+                  "color": "#dfd2ae"
+                }
+              ]
+            },
+            {
+              "featureType": "poi",
+              "elementType": "geometry",
+              "stylers": [
+                {
+                  "color": "#dfd2ae"
+                }
+              ]
+            },
+            {
+              "featureType": "poi",
+              "elementType": "labels.text.fill",
+              "stylers": [
+                {
+                  "color": "#93817c"
+                }
+              ]
+            },
+            {
+              "featureType": "poi.business",
+              "stylers": [
+                {
+                  "visibility": "off"
+                }
+              ]
+            },
+            {
+              "featureType": "poi.government",
+              "stylers": [
+                {
+                  "visibility": "off"
+                }
+              ]
+            },
+            {
+              "featureType": "poi.medical",
+              "stylers": [
+                {
+                  "visibility": "off"
+                }
+              ]
+            },
+            {
+              "featureType": "poi.park",
+              "elementType": "geometry.fill",
+              "stylers": [
+                {
+                  "color": "#a5b076"
+                }
+              ]
+            },
+            {
+              "featureType": "poi.park",
+              "elementType": "labels.text.fill",
+              "stylers": [
+                {
+                  "color": "#447530"
+                }
+              ]
+            },
+            {
+              "featureType": "poi.place_of_worship",
+              "stylers": [
+                {
+                  "visibility": "off"
+                }
+              ]
+            },
+            {
+              "featureType": "poi.school",
+              "stylers": [
+                {
+                  "visibility": "off"
+                }
+              ]
+            },
+            {
+              "featureType": "poi.school",
+              "elementType": "geometry.fill",
+              "stylers": [
+                {
+                  "visibility": "off"
+                }
+              ]
+            },
+            {
+              "featureType": "poi.school",
+              "elementType": "geometry.stroke",
+              "stylers": [
+                {
+                  "visibility": "off"
+                }
+              ]
+            },
+            {
+              "featureType": "poi.school",
+              "elementType": "labels.icon",
+              "stylers": [
+                {
+                  "visibility": "off"
+                }
+              ]
+            },
+            {
+              "featureType": "poi.school",
+              "elementType": "labels.text",
+              "stylers": [
+                {
+                  "visibility": "off"
+                }
+              ]
+            },
+            {
+              "featureType": "poi.school",
+              "elementType": "labels.text.fill",
+              "stylers": [
+                {
+                  "visibility": "off"
+                }
+              ]
+            },
+            {
+              "featureType": "poi.school",
+              "elementType": "labels.text.stroke",
+              "stylers": [
+                {
+                  "visibility": "off"
+                }
+              ]
+            },
+            {
+              "featureType": "poi.sports_complex",
+              "stylers": [
+                {
+                  "visibility": "off"
+                }
+              ]
+            },
+            {
+              "featureType": "road",
+              "elementType": "geometry",
+              "stylers": [
+                {
+                  "color": "#f5f1e6"
+                }
+              ]
+            },
+            {
+              "featureType": "road.arterial",
+              "elementType": "geometry",
+              "stylers": [
+                {
+                  "color": "#fdfcf8"
+                }
+              ]
+            },
+            {
+              "featureType": "road.highway",
+              "stylers": [
+                {
+                  "visibility": "off"
+                }
+              ]
+            },
+            {
+              "featureType": "road.highway",
+              "elementType": "geometry",
+              "stylers": [
+                {
+                  "color": "#f8c967"
+                }
+              ]
+            },
+            {
+              "featureType": "road.highway",
+              "elementType": "geometry.stroke",
+              "stylers": [
+                {
+                  "color": "#e9bc62"
+                }
+              ]
+            },
+            {
+              "featureType": "road.highway.controlled_access",
+              "elementType": "geometry",
+              "stylers": [
+                {
+                  "color": "#e98d58"
+                }
+              ]
+            },
+            {
+              "featureType": "road.highway.controlled_access",
+              "elementType": "geometry.stroke",
+              "stylers": [
+                {
+                  "color": "#db8555"
+                }
+              ]
+            },
+            {
+              "featureType": "road.local",
+              "elementType": "labels.text.fill",
+              "stylers": [
+                {
+                  "color": "#806b63"
+                }
+              ]
+            },
+            {
+              "featureType": "transit.line",
+              "elementType": "geometry",
+              "stylers": [
+                {
+                  "color": "#dfd2ae"
+                }
+              ]
+            },
+            {
+              "featureType": "transit.line",
+              "elementType": "labels.text.fill",
+              "stylers": [
+                {
+                  "color": "#8f7d77"
+                }
+              ]
+            },
+            {
+              "featureType": "transit.line",
+              "elementType": "labels.text.stroke",
+              "stylers": [
+                {
+                  "color": "#ebe3cd"
+                }
+              ]
+            },
+            {
+              "featureType": "transit.station",
+              "elementType": "geometry",
+              "stylers": [
+                {
+                  "color": "#dfd2ae"
+                }
+              ]
+            },
+            {
+              "featureType": "water",
+              "elementType": "geometry.fill",
+              "stylers": [
+                {
+                  "color": "#b9d3c2"
+                }
+              ]
+            },
+            {
+              "featureType": "water",
+              "elementType": "labels.text.fill",
+              "stylers": [
+                {
+                  "color": "#92998d"
+                }
+              ]
+            }
+          ]
     }
     geocoder = new google.maps.Geocoder()
     map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
+    //Start geocode function once button is clicked
     $("#location").on("click", function () {
-    geocodeAddress(geocoder, map)
+        geocodeAddress(geocoder, map)
+    });
+
+}
+
+
+//transform location into latlng
+function geocodeAddress(geocoder, resultsMap) {
+    address = $("#locationSelected").val();
+    geocoder.geocode({ 'address': address }, function (results, status) {
+        if (status === 'OK') {
+            resultsMap.setCenter(results[0].geometry.location);
+
+            var marker = new google.maps.Marker({
+                map: resultsMap,
+                position: results[0].geometry.location
+            });
+
+            //call for lodging locations
+            infowindow = new google.maps.InfoWindow();
+            service = new google.maps.places.PlacesService(map);
+            service.textSearch({
+                location: results[0].geometry.location,
+                radius: 10000,
+                type: ['lodging']
+            }, detailsCallback);
+
+        } else {
+            console.log('Geocode was not successful for the following reason: ' + status);
+        }
+
     });
 }
 
-function geocodeAddress(geocoder, resultsMap) {
-    address = $("#locationSelected").val();
-    geocoder.geocode({'address': address}, function(results, status) {
-      if (status === 'OK') {
-        resultsMap.setCenter(results[0].geometry.location);
-        var marker = new google.maps.Marker({
-          map: resultsMap,
-          position: results[0].geometry.location
-        });
-      } else {
-        alert('Geocode was not successful for the following reason: ' + status);
-      }
+//create entries in the map
+function detailsCallback(results, status) {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+        for (var i = 0; i < results.length; i++) {
+            var request = {
+                placeId: results[i].place_id
+            };
+
+            service.getDetails(request, placeDetailsCallback);
+
+            function placeDetailsCallback(place, status) {
+                if (status == google.maps.places.PlacesServiceStatus.OK) {
+                    createMarker(place);
+                }
+            }
+
+        }
+    }
+
+}
+
+//add markers in the map
+function createMarker(place) {
+    var placeLoc = place.geometry.location;
+    var marker = new google.maps.Marker({
+        map: map,
+        position: place.geometry.location,
+        title: place.name,
     });
-  }
 
-  
+    //expand marker when clicked
+    google.maps.event.addListener(marker, 'click', function () {
+        if (place.name != undefined) {
+            var hotelName = "<p><strong> " + place.name + "</strong></p>";
+        }
+        else {
+            var hotelName = "";
+        }
 
+        if (place.formatted_address != undefined) {
+            var hotelAddress = "<p>Hotel Address: " + place.formatted_address + "</br>";
+        }
+        else {
+            var hotelAddress = "";
+        }
+        
+        if (place.international_phone_number != undefined) {
+            var hotelPhone = "Hotel Phone Number: " + place.international_phone_number + "</br>";
+        }
+        else {
+            var hotelPhone = "";
+        }
 
+        if (place.rating != undefined) {
+            var hotelRating = "Hotel Rating: " + place.rating + " out of 5</p>";
+        }
+        else {
+            var hotelRating = "</p>";
+        }
 
+       
+        if (place.website != undefined) {
+            var bookHotel = " <a href=" + place.website + " target ='_blank'> Book Online</a></p>";
+        }
+        else {
+            var bookHotel = "";
+        }
 
-//})
+        infowindow.setContent(hotelName + hotelAddress + hotelPhone + hotelRating + bookHotel);
+        infowindow.open(map, this);
+    });
+}
+
